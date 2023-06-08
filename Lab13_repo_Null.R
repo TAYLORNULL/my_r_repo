dc6e44fd3d7476739df4d5c7862e7a904241d1d8
TAYLORNULL <tbone.null@gmail.com>
2023-06-08 15:41:12 -0600

    change

--- Lab13_repo_Null.R
+++ Lab13_repo_Null.R
@@ -1,4 +1,4 @@
-### Lab 13 3
+### Lab 13
 
 x = rnorm(100, mean = 20, sd = 5)
 y = x + rnorm(100, mean = 0, sd = 2)
@@ -8,3 +8,12 @@ fit = lm(y ~ x)
 summary(fit)
 
 
+plot(x,y)
+abline(fit, col=2)
+
+getwd() 
+
+co2 = read.csv("co2_mm_mlo.csv")
+plot(co2$decdate, co2$interpolated, type='l', main="Mauna Loa CO2",
+     xlab="Time", ylab="ppm")
+lines(co2$decdate, co2$trend, col=2, lwd=2)
